a2b66372428cadf69dfa8d9cbf46b70bce0be53c
momo <zyongzhu24@gmail.com>
2023-05-24 22:51:16 +0800

    Accurately catch errors

--- exception_error.py
+++ exception_error.py
@@ -1,15 +1,28 @@
-num = input('請輸入一個數字： ')
+# num = input('請輸入一個數字： ')
 
-try: # 捕捉所有錯誤
-    # 放有可能發生錯誤的程式碼
-    int(num)
-except:
-    print('caught an error')
+# try: # 捕捉所有錯誤
+#     # 放有可能發生錯誤的程式碼
+#     int(num)
+# except:
+#     print('caught an error')
 
-print('program can still run')
+# print('program can still run')
 
-# 請輸入一個數字： q
-# caught an error
-# program can still run
+# # 請輸入一個數字： q
+# # caught an error
+# # program can still run
+
+
+try:
+    with open('file.txt','r') as f:
+        for line in f:
+            int(line.strip())
+except FileNotFoundError:
+    print('File not found!')
+except ValueError:
+    print('Could not convert to int')
+
+    
+
 
 
