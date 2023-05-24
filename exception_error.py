3b2026372eb3d905a040147b7ffd743ef16b616e
momo <zyongzhu24@gmail.com>
2023-05-25 00:16:53 +0800

    while True example

--- exception_error.py
+++ exception_error.py
@@ -13,22 +13,35 @@
 # # program can still run
 
 
-try:
-    with open('file.txt','r') as f:
-        for line in f:
-            int(line.strip())
-except FileNotFoundError as err:
-    print(err)
-    # [Errno 2] No such file or directory: 'file.txt'
-except ValueError as err:
-    print(err)
-    # invalid literal for int() with base 10: '嗨'
-else:
-    print('great! 都沒有發生錯誤')
-finally:
-    print('in finally')
+# try:
+#     with open('file.txt','r') as f:
+#         for line in f:
+#             int(line.strip())
+# except FileNotFoundError as err:
+#     print(err)
+#     # [Errno 2] No such file or directory: 'file.txt'
+# except ValueError as err:
+#     print(err)
+#     # invalid literal for int() with base 10: '嗨'
+# else:
+#     print('great! 都沒有發生錯誤')
+# finally:
+#     print('in finally')
 
-
+count = 0
+while True:
+    try:
+        num = input('please enter a number: ')
+        num = int(num)
+        print(f'great! you entered {num}')
+    except ValueError as err:
+        count += 1
+        if count < 3:
+            print(f'已輸入錯誤{count}次')
+            print(f'caught an ValueError : {err}')
+        else:
+            print('已輸入錯誤三次，不給輸入了')
+            break
 
 
 
